9e08f23f0535e0c04cff3d2a32b7c10c18a81971
Galib Mehboob <86629449+Galibmehboob@users.noreply.github.com>
2026-07-29 03:31:36 +0600

    bug fixing auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_URL + "/api/auth",
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
 
   plugins: [
     inferAdditionalFields({
