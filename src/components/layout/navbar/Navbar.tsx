4ff7e3d385c2e17a9926b4e1464ff8b74a44cecf
Galib Mehboob <86629449+Galibmehboob@users.noreply.github.com>
2026-07-29 03:37:31 +0600

    bug fixing auth

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -14,12 +14,12 @@ import {
   Dropdown,
 } from "@heroui/react";
 
-import { authClient } from "@/lib/auth-client";
+import { authClient, useSession } from "@/lib/auth-client";
 
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
 import MobileMenu from "./MobileMenu";
-import { useSession } from "@/hooks/useSession";
+
 
 export default function Navbar() {
   const router = useRouter();
